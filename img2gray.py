846ee4f7a4c60587a02fe4d77d85a48097173867
BBLG <eric558994@gmail.com>
2021-07-13 13:20:49 +0800

    圖片前處理

---
+++ img2gray.py
@@ -0,0 +1,13 @@
+from glob import glob
+import os
+import cv2
+
+paths = glob(os.path.join('dict/','*'))
+
+for path in paths:
+    img = cv2.imread(path)
+
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img,(512,512))
+
+    cv2.imwrite(path,img)
